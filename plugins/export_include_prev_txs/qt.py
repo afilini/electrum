0b65016eaac747ed9653572c27c03f9ad018f8d0
Alekos Filini <alekos.filini@gmail.com>
2018-03-05 22:52:06 +0100

    Add a menu entry to load a TX that contains previous TXs

--- plugins/export_include_prev_txs/qt.py
+++ plugins/export_include_prev_txs/qt.py
@@ -24,8 +24,10 @@
 # SOFTWARE.
 
 import json
+import os
 
 from PyQt5.QtWidgets import QPushButton
+from electrum import Transaction
 from electrum.i18n import _
 from electrum.plugins import BasePlugin, hook
 
@@ -33,19 +35,6 @@ from electrum.plugins import BasePlugin, hook
 class Plugin(BasePlugin):
     def __init__(self, parent, config, name):
         BasePlugin.__init__(self, parent, config, name)
-
-    @hook
-    def init_qt(self, gui):
-        for window in gui.windows:
-            self.on_new_window(window)
-
-    @hook
-    def on_new_window(self, window):
-        self.update(window)
-
-    @hook
-    def on_close_window(self, window):
-        self.update(window)
 
     def is_available(self):
         return True
@@ -62,6 +51,11 @@ class Plugin(BasePlugin):
         if not d.wallet.network:
             d.export_prev_txs_button.hide()
             return
+
+    @hook
+    def init_menubar_tools(self, main_window, tools_menu):
+        main_window.raw_transaction_menu.addAction(_("&For offline signing"),
+                                                   lambda: load_with_prev_txs(main_window))
 
     def do_export(self, d):
         name = 'signed_%s.txn' % (d.tx.txid()[0:8]) if d.tx.is_complete() else 'unsigned.txn'
@@ -91,3 +85,44 @@ class Plugin(BasePlugin):
 
             d.show_message(_("Transaction exported successfully"))
             d.saved = True
+
+
+def load_with_prev_txs(main_window):
+    tx = read_tx_from_file(main_window)
+    if tx:
+        main_window.show_transaction(tx)
+
+
+def tx_from_str(txt):
+    txt = txt.strip()
+    if not txt:
+        raise ValueError(_("Empty string"))
+
+    tx_dict = json.loads(str(txt))
+
+    assert "hex" in tx_dict.keys()
+    assert "prev_txs" in tx_dict.keys()
+
+    return tx_dict["hex"], tx_dict.get("prev_txs", {})
+
+
+def read_tx_from_file(main_window):
+    file_name = main_window.getOpenFileName(_("Select your transaction file"), "*.txn")
+
+    if not file_name:
+        return
+    try:
+        with open(file_name, "r") as f:
+            file_content = f.read()
+    except (ValueError, IOError, os.error) as reason:
+        main_window.show_critical(_("Electrum was unable to open your transaction file") + "\n" + str(reason),
+                                  title=_("Unable to read file or no transaction found"))
+        return
+
+    tx_hex, prev_txs_hex = tx_from_str(file_content)
+
+    tx = Transaction(tx_hex)
+    for txin in tx.inputs():
+        txin["prev_tx"] = Transaction(prev_txs_hex.get(txin["prevout_hash"]))
+
+    return tx
